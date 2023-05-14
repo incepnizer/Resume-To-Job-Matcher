12971a325e00d04612cabeaca45aee571abbb5c7
Aksh Tyagi <84888735+incepnizer@users.noreply.github.com>
2023-05-14 13:59:08 -0400

    Updated script to interface with new UI

--- script.js
+++ script.js
@@ -1,54 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const exp = document.getElementById("exp");
-    const weblink = document.getElementById("weblink");
-    const submitButton = document.getElementById("submit");
-    
-    submitButton.addEventListener('click', () => {
-        const expText = exp.value;
-        let linkToScrap = weblink.value;
-
-        // conditions before continuing
-        if (expText === "") {
-            alert("Please enter something in the experience field");
-        } else if (linkToScrap === "") {
-            alert("Please enter a link to get job description data from");
-        } else if (linkToScrap.includes("glassdoor") === false) {
-            alert("Please enter a valid job posting website");
-        } else {
-            // scrape then AI time
-            let xhr = new XMLHttpRequest();
-            linkToScrap = "https://cors-anywhere.herokuapp.com/" + linkToScrap;
-            xhr.open("GET", linkToScrap, true);
-            xhr.responseType = "document";
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    let response = xhr.responseXML.querySelectorAll(".JobDescriptionContent.desc");
-                    console.log(response);
-                } 
-            };
-            //JobDescriptionContainer
-            //JobDescriptionContent desc
-            //xhr.send();
-        }
-    })
-})
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    let currentJob = "";
-    console.log("whats");
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, jobID } = obj;
-        if (type === "NEW") {
-            currentJob = jobID;
-            newJobLoaded();
-        }
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        // Your form handling code here
     });
+    const workExp = document.getElementById('exp');
+    const jobDesc = document.getElementById('descp');
+    const next = document.getElementById('submit');
 
-    const newJobLoaded = () => {
+    next.addEventListener('click', () => {
+        const workText = workExp.value;
+        const jobDe = jobDesc.value;
 
-    }
-})
+        if (workText === '' || jobDe === '') {
+            alert("You've left some fields empty");
+        } else {
+            // using different api
+            async function getData() {
+                const url = `https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?text1=${workText}&text2=${jobDe}`;
+
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': '0ccb5993c7msh14635afbbe6740ep1ef1bdjsn851236cc04e0',
+                        'X-RapidAPI-Host': 'twinword-text-similarity-v1.p.rapidapi.com'
+                    }
+                };
+                
+                try {
+                    const response = await fetch(url, options);
+                    const result = await response.text();
+                    console.log(result);
+
+                    let end = 0;
+                    
+                    for (let i = 13; i < result.length; i++) {
+                        if (result.substring(i, i + 1) === ',') {
+                            end = i;
+                            break;
+                        }
+                    }
+
+                    let score = 100 * Number(result.substring(14, end));
+
+                    switch (true) {
+                        case (score <= 10):
+                            score = score + " - your resume doesn't match too well and you should consider adding qualifications the company is looking for.";
+                            break;
+                        case (score <= 50):
+                            score = score + " - your resume matches decently well, and you have a decent chance at getting an interview";
+                            break;
+                        case (score <= 100):
+                            score = score + " - you have many qualifications the company is looking for and have a good change of landing the internship given a decent to good interview";
+                    }
+                    document.getElementById("result").innerHTML = score;
+                    //console.log(score);
 
 
+                } catch (error) {
+                }
+            }
+
+            getData();
+
+        }
+
+    });
+});
 
